470db8c0dadcf955081425354ee3eb4df9935cc0
siwenhu <siwenhu@163.com>
2016-05-09 11:04:08 +1000

    use thread module

--- main.py
+++ main.py
@@ -1,256 +1,139 @@
-#coding:utf-8
-'''
-Created on 2015年5月13日
-
-@author: root
-'''
-from PyQt4.QtCore import QThread, SIGNAL, QByteArray, QTimer, QTime, QString, QMutex
-from PyQt4.QtNetwork import QUdpSocket, QHostAddress, QAbstractSocket
-from PyQt4.QtGui import QMessageBox, QApplication, QCursor
-import sys, gc
+# -*- coding: utf-8 -*-
+from PyQt4.QtGui import *
+from PyQt4.QtCore import *
+import sys
+from PyQt4.QtNetwork import *
+#from socketthreadtwo import SocketThread
+from socketthread import SocketThread
 import time
-import uuid
-import datetime
-import logging.handlers
-import os
-import traceback
 
-LOG_PATH = "/opt/morningcloud/massclouds/record.log"
+QTextCodec.setCodecForTr(QTextCodec.codecForName("utf8"))
 
-class SocketThread(QThread):
+class UdpClient(QWidget):
     def __init__(self,parent=None):
-        super(SocketThread,self).__init__(parent)
-        self.clientuuid = str(uuid.uuid4())
-        self.port = 5555
-        self.broadFlag = False 
-        self.currentStudent = False
-        self.porttwo = 5556
+        super(UdpClient,self).__init__(parent)
+        self.setWindowFlags(Qt.FramelessWindowHint|Qt.WindowStaysOnTopHint|Qt.Dialog)
         
-        self.framedata = {}
-        self.currentframe = ""
+        self.pale = QPalette(Qt.black)
+        #self.setAutoFillBackground(True)
+        self.setPalette(self.pale)
         
-        self.dataframelist = {}
-        self.joinGroupTwo = False
+        self.broadflag = False
         
-        self.udpSocket = QUdpSocket(self)
-        self.udpSocket.setReadBufferSize(1024*1024)
         
-        #self.connect(self.udpSocket,SIGNAL("readyRead()"),self.dataReceive)
-        self.connect(self.udpSocket,SIGNAL("readyRead()"),self.dataReceiveTest)
-        self.results = self.udpSocket.bind(self.port)
-        self.mcast_addr = QHostAddress("224.0.0.17")
+        self.setFixedSize(QSize(QApplication.desktop().width(),QApplication.desktop().height()))
+        #self.setAttribute(Qt.WA_TranslucentBackground,True)
         
-        self.mcast_addr_two = QHostAddress("224.0.0.18")
-        self.udpSocketTwo = QUdpSocket(self)
-        self.udpSocketTwo.setReadBufferSize(1024)
-        self.connect(self.udpSocketTwo,SIGNAL("readyRead()"),self.dataReceiveTwo)
-        self.result = self.udpSocketTwo.bind(self.porttwo)
-        if self.result:
-            self.joinGroupTwo = self.udpSocketTwo.joinMulticastGroup(self.mcast_addr_two)
-            print("joinmulticastGroup %d" % self.joinGroupTwo)
-            
-       
-        if not os.path.exists(os.path.dirname(LOG_PATH)):
-            os.makedirs(os.path.dirname(LOG_PATH))
-        handler = logging.handlers.RotatingFileHandler(LOG_PATH, maxBytes = 1024*1024, backupCount = 5) # ?????????handler
-        fmt = '%(asctime)s - %(filename)s:%(lineno)s - %(name)s - %(message)s'  
-		       
-        formatter = logging.Formatter(fmt)   # ?????????formatte
-        handler.setFormatter(formatter)      # ???handler??????format
-
-        self.logger = logging.getLogger(LOG_PATH)    # ????????tst???logger 
-        self.logger.addHandler(handler)           # ???logger??????handle
-        self.logger.setLevel(logging.DEBUG)
-
-        self.timer = QTimer()
-        self.connect(self.timer, SIGNAL("timeout()"), self.bindUdpPort)
-        self.timer.start(1000)
-
-        self.mutex = QMutex()
-
-        gc.set_debug(gc.DEBUG_STATS|gc.DEBUG_LEAK)
+        self.showlabel = 0
+        
+        self.socketThread = SocketThread()
+        self.connect(self.socketThread, SIGNAL("imgsignal"),self.paintLabel)
+        #self.connect(self.socketThread, SIGNAL("receiveteacherip"),self.slotGetTeacherIp)
+        self.connect(self.socketThread, SIGNAL("startbroadcast"),self.slotStartBroadcast)
+        self.connect(self.socketThread, SIGNAL("stopbroadcast"),self.slotStopBroadcast)
+        #self.connect(self.socketThread, SIGNAL("mousepos"),self.slotSetMousePos)
+        self.imgstr = "000000"
+        self.framplat = "jpg"
+        self.imgLabel = QLabel(self)
+        self.imgLabel.setFixedSize(QSize(QApplication.desktop().width(),QApplication.desktop().height()))
+        
+        self.imgLabelTwo = QLabel(self)
+        #self.imgLabelTwo.setFixedSize(QSize(QApplication.desktop().width(),QApplication.desktop().height()))
+        #self.imgLabelTwo.lower()
+        
+        #self.mouseLabel = QLabel(self)
+        #self.mouseLabel.setFixedSize(QSize(30,30))
+        #self.mousepix = QPixmap("images/mousetwo.ico").scaled(30,30)
+        #self.mouseLabel.setPixmap(self.mousepix)
+        #self.mouseLabel.raise_()
+        
+#         self.scene = QGraphicsScene()
+#         view = QGraphicsView(self.scene,self)
+#         view.setFixedSize(QSize(1440,900))
+#         
+        #self.imgLabel.hide()
 
     def bindUdpPort(self):
-        if not self.joinGroupTwo: 
-            self.joinGroupTwo = self.udpSocketTwo.joinMulticastGroup(self.mcast_addr_two)
-            self.logger.info("joinGroupTwo:%d" % self.joinGroupTwo) 
-        else:
-            self.timer.stop()
-
-    def dataReceiveTest(self):
-        while self.udpSocket.hasPendingDatagrams():
-            try:
-                datagram = QByteArray()
-                datagram.resize(self.udpSocket.pendingDatagramSize())
-
-                self.udpSocket.readDatagram(datagram.size())
-
-            except Exception, e:
-                #del datacontent 
-                f = open("/opt/morningcloud/massclouds/record.txt", 'a')
-                traceback.print_exc(file=f)
-                f.flush()
-                f.close()
-                self.logger.error(e.message) 
-
-    def dataReceive(self):
-        while self.udpSocket.hasPendingDatagrams():
-            try:
-                datagram = QByteArray()
-                datagram.resize(self.udpSocket.pendingDatagramSize())
-
-                msglist = self.udpSocket.readDatagram(datagram.size())
-                if self.broadFlag == False:
-                    continue
-                if len(msglist[0]) <= 21:
-                    self.logger.info("msg data smaller") 
-                    continue
-                msg = msglist[0]
-                timetemp = msg[0:17]
-                datanumth = msg[17:19]
-                datatotalnum = msg[19:21]
-                datacontent = msg[21:]
-
-                self.addToLocal(timetemp,datanumth,datatotalnum,datacontent)
-
-                #del msg 
-                #del datacontent 
-            except Exception, e:
-                #del datacontent 
-                f = open("/opt/morningcloud/massclouds/record.txt", 'a')
-                traceback.print_exc(file=f)
-                f.flush()
-                f.close()
-                self.logger.error(e.message) 
-
-    def addToLocal(self,timetemp,datanumth,datatotalnum,datacontent):
-        try:
-            if len(self.framedata) > 30:
-                if int(datanumth) != int(datatotalnum) - 1:
-                    self.framedata.clear()
-                    return
-            if self.framedata.has_key(timetemp):
-                self.framedata[timetemp][datanumth] = datacontent
-                if len(self.framedata[timetemp]) == int(datatotalnum):
-                    self.mutex.lock()
-                    self.dataframelist[timetemp] = self.framedata[timetemp]
-                    self.mutex.unlock()
-                    self.framedata.pop(timetemp)
-                
-            else:
-               self.framedata[timetemp] = {}
-               self.framedata[timetemp][datanumth] = datacontent
-        except Exception, e:
-            self.mutex.unlock()
-            f = open("/opt/morningcloud/massclouds/record.txt", 'a')
-            traceback.print_exc(file=f)
-            f.flush()
-            f.close()
-            self.logger.error("addToLocal") 
-            self.logger.error(e.message) 
-            
-    def sortAddLocalList(self):
-        if len(self.dataframelist) > 8:
-            self.mutex.lock()
-            self.dataframelist.clear()
-            self.mutex.unlock()
-
-        try:
-            if len(self.dataframelist) > 2:
-                self.mutex.lock()
-                keylist = []
-                for key in self.dataframelist:
-                    keylist.append(int(key))
-                keylist.sort()
-                imgdata = ""
-                for i in range(0,len(self.dataframelist[("%017d"%(keylist[0]))])):
-                    keys = "%02d"%i
-                    imgdata = imgdata + self.dataframelist[("%017d"%(keylist[0]))][keys]
-
-                self.currentframe = imgdata
-                self.dataframelist.pop(("%017d"%(keylist[0])))
-                self.mutex.unlock()
-    
-                #del keylist 
-                #del imgdata
-            else:
-                self.currentframe = None
-
-        except Exception, e:
-            self.mutex.unlock()
-            f = open("/opt/morningcloud/massclouds/record.txt", 'a')
-            traceback.print_exc(file=f)
-            f.flush()
-            f.close()
-            self.logger.error(e.message) 
-            
-    def dataReceiveTwo(self):
-        while self.udpSocketTwo.hasPendingDatagrams():
-            datagram = QByteArray()
-            datagram.resize(self.udpSocketTwo.pendingDatagramSize())
-
-            msglist = self.udpSocketTwo.readDatagram(datagram.size())
-            msg = str(msglist[0])
-
-        self.parseMsg(msg)
+        self.socketThread.bindUdpPort()
         
-
-    def slotStartAllBroadcast(self,msgs):
-        result = self.udpSocket.joinMulticastGroup(self.mcast_addr)
-        self.logger.info("joinGroup:%d" % result) 
-        self.emit(SIGNAL("startbroadcast"))
-        self.broadFlag = True
-        self.start()
-
-    
+    def slotStartBroadcast(self):
+        self.broadflag = True
+        self.emit(SIGNAL("start"))
+        
+        
     def slotStopBroadcast(self):
-        self.udpSocket.leaveMulticastGroup(self.mcast_addr)
-        self.emit(SIGNAL("stopbroadcast"))
-        self.broadFlag = False
-        self.currentStudent = False
-        self.framedata.clear()
-        self.dataframelist.clear()
-        self.currentframe = None
-
-
-    def parseMsg(self,msg):
-        if msg.split("#")[0] == "startbroadcast":
-            print "startbroadcast"
-            self.logger.info("startbroadcast") 
-            self.slotStartAllBroadcast(msg)
-                
-        elif msg.split("#")[0] == "stopbroadcast":
-            print "stopbroadcast"
-            self.logger.info("stopbroadcast") 
-            result = self.udpSocket.leaveMulticastGroup(self.mcast_addr)
-                    
-            self.logger.info("leaveGroup:%d" % result) 
-            self.emit(SIGNAL("stopbroadcast"))
-            self.broadFlag = False
-                
-            self.mutex.lock()
-            self.framedata.clear()
-            self.dataframelist.clear()
-            self.mutex.unlock()
-            self.currentframe = None
-
-    def run(self):
-        while self.broadFlag:
-            self.sortAddLocalList()
-
-            if self.currentframe == None:
-                time.sleep(0.01)
-                continue
+        self.broadflag = False
+        self.emit(SIGNAL("stop"))
+        #self.pale.setColor(QPalette.Background,QColor(Qt.black))
+        self.imgLabelTwo.clear()
+        #self.imgLabelTwo.setAutoFillBackground(True)
+        #self.imgLabelTwo.setPalette(self.pale)
+        
+    def paintLabel(self,imgstr):
+#         pixmap = QPixmap()
+#         pixmap.loadFromData(imgstr, self.framplat)
+#         self.scene.addPixmap(pixmap)
+#         return
+        if self.broadflag:
+        
+            #if self.showlabel == 0:
+            #self.showlabel = 1
+            pixmap = QPixmap()
+            pixmap.loadFromData(imgstr, self.framplat)
             
-            #msg = self.currentframe
-            self.emit(SIGNAL("imgsignal"), self.currentframe)
-            #self.msginfo = msg
-            time.sleep(0.01)
-            #del self.currentframe
-    
+            
+            self.imgLabelTwo.resize(pixmap.size())
+            self.imgLabelTwo.move(QPoint((self.width() - self.imgLabelTwo.width())/2,(self.height() - self.imgLabelTwo.height())/2))
+            self.imgLabelTwo.setAlignment(Qt.AlignCenter)
+            self.imgLabelTwo.setPixmap(pixmap)
+            #self.imgLabel.lower()
+                
+    #         else:
+    #             #image0=QImage.fromData(imgstr,self.framplat)
+    #             #pixmap = QPixmap(image0).scaled(1366,768)
+    #             self.showlabel = 0
+    #             pixmap = QPixmap()
+    #             pixmap.loadFromData(imgstr, self.framplat)
+    #             self.imgLabel.setAlignment(Qt.AlignCenter)
+    #             self.imgLabel.setPixmap(pixmap)
+    #             self.imgLabelTwo.lower()
+    #             
+            
+            
+    #def slotSetMousePos(self,x,y):
+        
+    #    self.mouseLabel.move(self.imgLabelTwo.pos() + QPoint(x,y))
+        
+        
+    def slotGetTeacherIp(self,teacherip):
+        pass
+        
+    def slotButton(self):
+        self.close()
+        
+        
+class MainWindow(QWidget):
+    def __init__(self,parent=None):
+        super(MainWindow,self).__init__(parent)
+        self.setWindowTitle(u"UDP Client")
+        self.setWindowFlags(Qt.WindowStaysOnTopHint)
+        self.setFixedSize(QSize(300,300))
+        self.udpClient = UdpClient()
+        self.connect(self.udpClient, SIGNAL("start"),self.startB)
+        self.connect(self.udpClient, SIGNAL("stop"),self.stopB)
+        
+        #self.startB()
+        
+    def startB(self):
+        self.udpClient.show()
+        self.udpClient.setFixedSize(QSize(QApplication.desktop().width(),QApplication.desktop().height()))
+        self.udpClient.imgLabel.setFixedSize(QSize(QApplication.desktop().width(),QApplication.desktop().height()))
+        
+    def stopB(self):
+        self.udpClient.close()
+        
 if __name__ == "__main__":
-    app = QApplication(sys.argv)
-    thread = SocketThread()
+    app=QApplication(sys.argv)
+    dialog=MainWindow()
+    dialog.show()
     app.exec_()
-
-
---
+++ socketthread.py
@@ -0,0 +1,238 @@
+#coding:utf-8
+'''
+Created on 2015年5月13日
+
+@author: root
+'''
+from PyQt4.QtCore import QThread, SIGNAL, QByteArray, QTimer, QTime, QString, QMutex
+#from PyQt4.QtNetwork import QUdpSocket, QHostAddress, QAbstractSocket
+from PyQt4.QtGui import QMessageBox, QApplication, QCursor
+import sys, gc
+import time
+import uuid
+import datetime
+import logging.handlers
+import os, socket, thread
+import traceback
+
+LOG_PATH = "/opt/morningcloud/massclouds/record.log"
+
+class SocketThread(QThread):
+    def __init__(self,parent=None):
+        super(SocketThread,self).__init__(parent)
+        self.clientuuid = str(uuid.uuid4())
+        self.port = 5555
+        self.broadFlag = False 
+        self.currentStudent = False
+        self.porttwo = 5556
+        
+        self.framedata = {}
+        self.currentframe = ""
+        
+        self.dataframelist = {}
+        self.joinGroupTwo = False
+        
+        #self.udpSocket = QUdpSocket(self)
+        #self.udpSocket.setReadBufferSize(1024*1024)
+        self.udpSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP) 
+        self.udpSocket.setsockopt(socket.SOL_SOCKET, socket.SO_SNDBUF, 1024*1024)
+        self.udpSocket.bind(("224.0.0.17",self.port))
+        self.udpSocket.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, 255)
+        self.udpSocket.setsockopt(socket.IPPROTO_IP,
+        socket.IP_ADD_MEMBERSHIP,
+        socket.inet_aton("224.0.0.17") + socket.inet_aton("0.0.0.0"));
+        
+        #self.connect(self.udpSocket,SIGNAL("readyRead()"),self.dataReceive)
+        #self.results = self.udpSocket.bind(self.port)
+        #self.mcast_addr = QHostAddress("224.0.0.17")
+        
+        self.udpSocketTwo = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP) 
+        self.udpSocketTwo.setsockopt(socket.SOL_SOCKET, socket.SO_SNDBUF, 1024)
+        self.udpSocketTwo.bind(("224.0.0.18",self.porttwo))
+        self.udpSocketTwo.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, 255)
+        self.udpSocketTwo.setsockopt(socket.IPPROTO_IP,
+        socket.IP_ADD_MEMBERSHIP,
+        socket.inet_aton("224.0.0.18") + socket.inet_aton("0.0.0.0"));
+
+        #self.mcast_addr_two = QHostAddress("224.0.0.18")
+        #self.udpSocketTwo = QUdpSocket(self)
+        #self.udpSocketTwo.setReadBufferSize(1024)
+        #self.connect(self.udpSocketTwo,SIGNAL("readyRead()"),self.dataReceiveTwo)
+        #self.result = self.udpSocketTwo.bind(self.porttwo)
+        #if self.result:
+        #    self.joinGroupTwo = self.udpSocketTwo.joinMulticastGroup(self.mcast_addr_two)
+        #    print("joinmulticastGroup %d" % self.joinGroupTwo)
+            
+       
+        if not os.path.exists(os.path.dirname(LOG_PATH)):
+            os.makedirs(os.path.dirname(LOG_PATH))
+        handler = logging.handlers.RotatingFileHandler(LOG_PATH, maxBytes = 1024*1024, backupCount = 5) # ?????????handler
+        fmt = '%(asctime)s - %(filename)s:%(lineno)s - %(name)s - %(message)s'  
+		       
+        formatter = logging.Formatter(fmt)   # ?????????formatte
+        handler.setFormatter(formatter)      # ???handler??????format
+
+        self.logger = logging.getLogger(LOG_PATH)    # ????????tst???logger 
+        self.logger.addHandler(handler)           # ???logger??????handle
+        self.logger.setLevel(logging.DEBUG)
+
+        #self.timer = QTimer()
+        #self.connect(self.timer, SIGNAL("timeout()"), self.dataReceiveTwo)
+        #self.timer.start(100)
+
+        self.mutex = QMutex()
+
+        gc.set_debug(gc.DEBUG_STATS|gc.DEBUG_LEAK)
+        #self.dataReceiveTwo()
+        thread.start_new_thread(self.dataReceiveTwo, ())
+
+    def bindUdpPort(self):
+        if not self.joinGroupTwo: 
+            self.joinGroupTwo = self.udpSocketTwo.joinMulticastGroup(self.mcast_addr_two)
+            self.logger.info("joinGroupTwo:%d" % self.joinGroupTwo) 
+        else:
+            self.timer.stop()
+
+    def dataReceive(self):
+        try:
+            msglist,add = self.udpSocket.recvfrom(32*1024+21)
+            msg = msglist
+            timetemp = msg[0:17]
+            datanumth = msg[17:19]
+            datatotalnum = msg[19:21]
+            datacontent = msg[21:]
+
+            #print timetemp
+            #print datatotalnum 
+            #print datanumth 
+            self.addToLocal(timetemp,datanumth,datatotalnum,datacontent)
+
+            #del msg 
+            #del datacontent 
+        except Exception, e:
+            #del datacontent 
+            f = open("/opt/morningcloud/massclouds/record.txt", 'a')
+            traceback.print_exc(file=f)
+            f.flush()
+            f.close()
+            self.logger.error(e.message) 
+
+    def addToLocal(self,timetemp,datanumth,datatotalnum,datacontent):
+        try:
+            if len(self.framedata) > 30:
+                self.framedata.clear()
+                return
+            if self.framedata.has_key(timetemp):
+                self.framedata[timetemp][datanumth] = datacontent
+                if len(self.framedata[timetemp]) == int(datatotalnum):
+                    self.dataframelist[timetemp] = self.framedata[timetemp]
+                    self.framedata.pop(timetemp)
+                
+            else:
+               self.framedata[timetemp] = {}
+               self.framedata[timetemp][datanumth] = datacontent
+        except Exception, e:
+            f = open("/opt/morningcloud/massclouds/record.txt", 'a')
+            traceback.print_exc(file=f)
+            f.flush()
+            f.close()
+            self.logger.error("addToLocal") 
+            self.logger.error(e.message) 
+            
+    def sortAddLocalList(self):
+        if len(self.dataframelist) > 8:
+            self.dataframelist.clear()
+            return
+
+        try:
+            if len(self.dataframelist) > 0:
+                keylist = []
+                for key in self.dataframelist:
+                    keylist.append(int(key))
+                keylist.sort()
+                imgdata = ""
+                for i in range(0,len(self.dataframelist[("%017d"%(keylist[0]))])):
+                    keys = "%02d"%i
+                    imgdata = imgdata + self.dataframelist[("%017d"%(keylist[0]))][keys]
+
+                self.currentframe = imgdata
+                self.dataframelist.pop(("%017d"%(keylist[0])))
+    
+                #del keylist 
+                #del imgdata
+            else:
+                self.currentframe = None
+
+        except Exception, e:
+            f = open("/opt/morningcloud/massclouds/record.txt", 'a')
+            traceback.print_exc(file=f)
+            f.flush()
+            f.close()
+            self.logger.error(e.message) 
+            
+    def dataReceiveTwo(self):
+        while True:
+            msglist, addr = self.udpSocketTwo.recvfrom(64)
+
+            self.parseMsg(msglist)
+            time.sleep(0.01)
+        
+
+    def slotStartAllBroadcast(self,msgs):
+        #result = self.udpSocket.joinMulticastGroup(self.mcast_addr)
+        #self.logger.info("joinGroup:%d" % result) 
+        self.emit(SIGNAL("startbroadcast"))
+        self.broadFlag = True
+        self.start()
+
+    
+    def slotStopBroadcast(self):
+        self.udpSocket.leaveMulticastGroup(self.mcast_addr)
+        self.emit(SIGNAL("stopbroadcast"))
+        self.broadFlag = False
+        self.currentStudent = False
+        self.framedata.clear()
+        self.dataframelist.clear()
+        self.currentframe = None
+
+
+    def parseMsg(self,msg):
+        if msg.split("#")[0] == "startbroadcast":
+            print "startbroadcast"
+            self.logger.info("startbroadcast") 
+            self.slotStartAllBroadcast(msg)
+                
+        elif msg.split("#")[0] == "stopbroadcast":
+            print "stopbroadcast"
+            self.logger.info("stopbroadcast") 
+            #result = self.udpSocket.leaveMulticastGroup(self.mcast_addr)
+                    
+            #self.logger.info("leaveGroup:%d" % result) 
+            self.emit(SIGNAL("stopbroadcast"))
+            self.broadFlag = False
+                
+            self.framedata.clear()
+            self.dataframelist.clear()
+            self.currentframe = None
+
+    def run(self):
+        while self.broadFlag:
+            self.dataReceive()
+            self.sortAddLocalList()
+
+            if self.currentframe == None:
+                #time.sleep(0.01)
+                continue
+            
+            #msg = self.currentframe
+            self.emit(SIGNAL("imgsignal"), self.currentframe)
+            #self.msginfo = msg
+            #time.sleep(0.01)
+            #del self.currentframe
+    
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    thread = SocketThread()
+    app.exec_()
+
+
